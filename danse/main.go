befcdfbb20071960fd6fdcba7b38c5f02b59b802
Edgar Fournival <contact@edgar-fournival.fr>
2017-01-15 20:45:52 +0100

    première implémentation de l'algorithme Son/DFS (ne fonctionne pas)

--- danse/main.go
+++ danse/main.go
@@ -1,54 +1,72 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-	"sync/atomic"
-)
+import "fmt"
 
 const (
-	MAX_GENUS = 15
-	SIZE      = 3 * (MAX_GENUS - 1)
+	MAX_GENUS = 10
+	SIZE      = 3 * MAX_GENUS
 )
 
 type Monoid struct {
-	genus            int
-	lastRemovedIndex int
-	decs             [SIZE]bool
+	c int
+	g int
+	m int
+	d [SIZE]int
 }
 
-func initDecs() (res [SIZE]bool) {
-	res[0] = true
+func root() Monoid {
+	m := Monoid{c: 0, g: 0, m: 1}
 
-	for i := 1; i < SIZE-1; i++ {
-		res[i] = false
+	for i := 0; i < SIZE; i++ {
+		m.d[i] = 1 + i/2
+	}
+
+	return m
+}
+
+func son(s Monoid, x int) (sx Monoid) {
+	sx.c = x + 1
+	sx.g = s.g + 1
+
+	if x > s.m {
+		sx.m = s.m
+	} else {
+		sx.m = s.m + 1
+	}
+
+	sx.d = s.d
+
+	for y := x; y < SIZE; y++ {
+		if s.d[y-x] > 0 {
+			sx.d[y] = s.d[y] - 1
+		}
 	}
 
 	return
 }
 
-func explore(m Monoid, c *uint64, d chan bool) {
-	m.genus++
+func count() (n [11]int) {
+	stack := NewStack()
+	stack.Push(root())
 
-	if m.genus > MAX_GENUS {
-		d <- true
+	for !stack.IsEmpty() {
+		s := stack.Pop()
+
+		n[s.g]++
+
+		if s.g < MAX_GENUS {
+			for x := s.c; x < s.c+s.m; x++ {
+				if s.d[x] == 1 {
+					stack.Push(son(s, x))
+				}
+			}
+		}
 	}
 
-	for i := 0; i < m.genus; i++ {
-		atomic.AddUint64(c, 1)
-		go explore(m, c, d)
-	}
+	return
 }
 
 func main() {
-	fmt.Printf("Maximum genus: %d\nMaximum procs to be spawned: %d\n", MAX_GENUS, runtime.GOMAXPROCS(0))
-
-	var count uint64 = 0
-	var done = make(chan bool)
-
-	explore(Monoid{1, 0, initDecs()}, &count, done)
-
-	if <-done {
-		fmt.Printf("Finished computation: %d\n", count)
-	}
+	//fmt.Printf("Maximum genus: %d\nMaximum procs to be spawned: %d\n", MAX_GENUS, runtime.GOMAXPROCS(0))
+	fmt.Printf("%+v\n", count())
 }
