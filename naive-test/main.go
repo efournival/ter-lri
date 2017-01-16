953ccad18283d38308f1520264579258ab170621
Edgar Fournival <contact@edgar-fournival.fr>
2017-01-16 22:05:54 +0100

    correction de l'algo naif (ne fonctionne toujours pas...)

--- naive-test/main.go
+++ naive-test/main.go
@@ -10,13 +10,12 @@ const (
 
 type Monoid struct {
 	g   int
-	m   int
 	d   [SIZE]bool
 	gen []int
 }
 
 func root() Monoid {
-	m := Monoid{g: 0, m: 1}
+	m := Monoid{g: 0}
 
 	for i := 0; i < SIZE; i++ {
 		m.d[i] = true
@@ -28,17 +27,48 @@ func root() Monoid {
 }
 
 func son(s Monoid, x int) (sx Monoid) {
+	// x MUST BE a generator of the source monoid
+	isGenerator := false
+
+	for _, v := range s.gen {
+		if v < x {
+			// We only need generators before x to compute this new monoid
+			sx.gen = append(sx.gen, v)
+		} else if v == x {
+			// Little check for more safety
+			isGenerator = true
+			// Break is OK because numbers are in the ascending order
+			break
+		}
+	}
+
+	if !isGenerator {
+		panic(fmt.Sprintf("ERROR: %d is not a generator\n", x))
+	}
+
 	sx.g = s.g + 1
 	sx.d = s.d
+	// Remove x
 	sx.d[x] = false
-	sx.m = x
 
-	for g := s.m; g < SIZE; g++ {
+	// We want to find new generators AFTER the generator we just removed
+	for g := x + 1; g < SIZE; g++ {
+		// If the number is in the semigroup
 		if sx.d[g] {
-			for i := 1; i < (g-1)/2; i++ {
-				if sx.d[i] && sx.d[g-1] {
-					sx.gen = append(sx.gen, g)
+			found := false
+
+			// We are looking for non-generator numbers
+			for i := 1; i <= g/2; i++ {
+				if sx.d[i] && sx.d[g-i] {
+					// g is a decomposition number, not a generator
+					found = true
+					break
 				}
+			}
+
+			if !found {
+				// If g is not a decomposition number, we have found a generator
+				sx.gen = append(sx.gen, g)
 			}
 		}
 	}
