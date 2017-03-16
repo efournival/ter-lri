44cf5ae234d0c6eae1f9faa57a6197fd996036af
Edgar Fournival <contact@edgar-fournival.fr>
2017-03-16 19:29:43 +0100

    premier jet (NON FONCTIONNEL) de boucle principale en Go pur : problème de synchronisation

--- danse/main.go
+++ danse/main.go
@@ -1,30 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
 
-func count() (n [COUNT_SIZE]int) {
-	stack := NewStack()
-	stack.Push(RootMonoid())
+	"github.com/efournival/ter-lri/go-numeric-monoid"
+)
 
-	for !stack.IsEmpty() {
-		s := stack.Pop()
+const (
+	MAX_GENUS   = 35
+	STACK_BOUND = 11
+)
 
-		n[s.g]++
-
-		if s.g < MAX_GENUS {
-			// < or <= ?
-			for x := s.c; x < s.c+s.m; x++ {
-				if s.d[x] == 1 {
-					stack.Push(s.GetSon(x))
-				}
-			}
-		}
-	}
-
-	return
-}
+var r nm.MonoidResults
 
 func main() {
-	//fmt.Printf("Maximum genus: %d\nMaximum procs to be spawned: %d\n", MAX_GENUS, runtime.GOMAXPROCS(0))
-	fmt.Printf("%+v\n", count())
+	m := nm.NewMonoid()
+	WalkChildren(m)
+	fmt.Printf("Got:\n\t%v\n", r)
+}
+
+func WalkChildren(m nm.GoMonoid) {
+	var wg sync.WaitGroup
+
+	if m.Genus() < MAX_GENUS-STACK_BOUND {
+		it := m.NewIterator()
+		var nbr uint64 = 0
+
+		for it.MoveNext() {
+			wg.Add(1)
+			go func(gen uint) {
+				WalkChildren(m.RemoveGenerator(gen))
+				wg.Done()
+			}(it.GetGen())
+			nbr++
+		}
+
+		wg.Wait()
+		atomic.AddUint64(&r[m.Genus()], nbr)
+	} else {
+		m.WalkChildrenStack(&r)
+	}
+
+	m.Free()
 }
